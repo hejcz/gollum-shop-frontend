0e18743ad7cdbfe444253b61c64a6de64d0607fa
hejcz <rubin94@gmail.com>
2022-07-10 20:44:20 +0200

    change api_url

--- src/stores.ts
+++ src/stores.ts
@@ -8,8 +8,7 @@ export const user_uuid: Writable<string> = writable(null);
 export const access_token: Writable<string> = writable(null);
 
 export const api_url: Readable<string> = readable(
-  "https://bonestest.herokuapp.com/api/"
-  // "http://localhost:3000/api/"
+  "https://bones.usermd.net/api/"
 );
 
 export function switchToLoggedUser() {
