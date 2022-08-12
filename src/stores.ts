7f7252b6a3cc16ae5513ac759e13d8bda980f2ca
Piotr Cieslak <piotr.cieslak.consultant@nielsen.com>
2022-08-12 21:40:15 +0200

    local testing

--- src/stores.ts
+++ src/stores.ts
@@ -8,7 +8,7 @@ export const user_uuid: Writable<string> = writable(null);
 export const access_token: Writable<string> = writable(null);
 
 export const api_url: Readable<string> = readable(
-  "https://bones.usermd.net/api/"
+  "http://localhost:3000/api/"
 );
 
 export function switchToLoggedUser() {
